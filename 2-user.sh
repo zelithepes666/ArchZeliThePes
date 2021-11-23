776acd29d94e69e7f93b9c5a673e2eef46e04333
zelithepes666 <94754607+zelithepes666@users.noreply.github.com>
2021-11-23 18:29:44 +0100

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -25,18 +25,11 @@ ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 PKGS=(
 'autojump'
 'awesome-terminal-fonts'
-'brave-bin' # Brave Browser
+'firefox'
 'dxvk-bin' # DXVK DirectX to Vulcan
 'github-desktop-bin' # Github Desktop sync
 'lightly-git'
 'lightlyshaders-git'
-'mangohud' # Gaming FPS Counter
-'mangohud-common'
-'nerd-fonts-fira-code'
-'nordic-darker-standard-buttons-theme'
-'nordic-darker-theme'
-'nordic-kde-git'
-'nordic-theme'
 'noto-fonts-emoji'
 'papirus-icon-theme'
 'plasma-pa'
@@ -47,7 +40,6 @@ PKGS=(
 'ttf-hack'
 'ttf-meslo' # Nerdfont package
 'ttf-roboto'
-'zoom' # video conferences
 'snap-pac'
 )
 
